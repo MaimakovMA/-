732363c103a923a606b909d2bc23cafd477830ef
Mikhail <Maimakov.ma@gmail.com>
2024-04-22 23:55:45 +0300

    Ура

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -56,9 +56,7 @@ export function Cards({ pairsCount = 3, previewSeconds = 5 }) {
   // Количество ошибок в режиме игры до трех ошибок
   const errors = useSelector(state => state.game.errors);
   // Статус режима игры до трех ошибок
-  // const setIsActiveGameMode = useSelector(state => state.game.isActiveGameMode);
-
-  const currentLevel = useSelector(state => state.game.currentLevel);
+  const isActiveGameMode = useSelector(state => state.game.isActiveGameMode);
   // Доступно ли использование прозрения
   const [isEpiphanyAvailable, setIsEpiphanyAvailable] = useState(true);
   // Доступно ли использование алохоморы
@@ -168,7 +166,7 @@ export function Cards({ pairsCount = 3, previewSeconds = 5 }) {
     if (playerLost) {
       dispatch(updateErrors());
 
-      if (!currentLevel === 3) {
+      if (!isActiveGameMode) {
         finishGame(STATUS_LOST);
       } else {
         const updatedCards = nextCards.map(card => {
